e0cd07eb70635d7d4a2651d3d9ddacdc69be5100
stako <stako@users.noreply.github.com>
2023-03-20 16:53:33 -0700

    add choice and playRound funcs

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,64 @@
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+
+    switch(num) {
+        default:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+    }
+}
+
+function getPlayerChoice() {
+    let choice = prompt("Enter your choice:");
+    choice = choice.toLowerCase();
+    return choice;
+}
+
+function playRound(playerChoice, computerChoice) {
+    let result;
+
+    switch(playerChoice) {
+        case 'rock':
+            switch(computerChoice) {
+                case 'rock':
+                    result = 'tie';
+                    break;
+                case 'paper':
+                    result = 'lose';
+                    break;
+                case 'scissors':
+                    result = 'win';
+            }
+            break;
+        case 'paper':
+            switch(computerChoice) {
+                case 'rock':
+                    result = 'win';
+                    break;
+                case 'paper':
+                    result = 'tie';
+                    break;
+                case 'scissors':
+                    result = 'lose';
+            }
+            break;
+        case 'scissors':
+            switch(computerChoice) {
+                case 'rock':
+                    result = 'lose';
+                    break;
+                case 'paper':
+                    result = 'win';
+                    break;
+                case 'scissors':
+                    result = 'lose';
+            }
+    }
+
+    return result;
+}
