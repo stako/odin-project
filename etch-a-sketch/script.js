04c103428165832f64b7b45736d12471b89406c2
stako <stako@users.noreply.github.com>
2023-03-25 15:35:42 -0700

    Add reset button

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,5 +1,7 @@
 const gridSize = 16; // how many columns (and rows) in the grid
 
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', resetSquares);
 
 const container = document.querySelector('#container');
 const totalSquares = gridSize**2;
@@ -14,4 +16,10 @@ for (let i = 1; i <= totalSquares; i++) {
 
 function onMouseover() {
     this.classList.replace('blank-square', 'filled-square');
+}
+
+function resetSquares() {
+    const filledSquares = document.querySelectorAll('.filled-square');
+
+    filledSquares.forEach((square) => square.classList.replace('filled-square', 'blank-square'));
 }
