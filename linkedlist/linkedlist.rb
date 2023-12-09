1806c7fdf162acb1121ec6f163198710f9881e1c
stako <stako@users.noreply.github.com>
2023-12-08 17:03:44 -0800

    first commit for linked list

---
+++ linkedlist/linkedlist.rb
@@ -0,0 +1,43 @@
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+class LinkedList
+  attr_reader :head
+
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+    return @head = Node.new(value) if @head.nil?
+
+    tmp = @head
+    tmp = tmp.next_node until tmp.next_node.nil?
+    tmp.next_node = Node.new(value)
+  end
+
+  def prepend(value)
+    tmp = Node.new(value)
+    tmp.next_node = @head unless @head.nil?
+    @head = tmp
+  end
+
+  def size
+    return 0 if @head.nil?
+
+    tmp = @head
+    size = 1
+    until tmp.next_node.nil?
+      tmp = tmp.next_node
+      size += 1
+    end
+
+    size
+  end
+end
