fa5274b1b21fe31fbf941d3b10432aef2f55fe70
stako <stako@users.noreply.github.com>
2024-03-04 15:12:48 -0800

    refactor: implement rubocop recommendations

--- ruby-exercises/caesar_cipher/caesar_cipher.rb
+++ ruby-exercises/caesar_cipher/caesar_cipher.rb
@@ -1,5 +1,7 @@
-def caesar_cipher(message, offset)
-  cipher = ''
+# frozen_string_literal: true
+
+def caesar_cipher(message, offset) # rubocop:disable Metrics/MethodLength
+  cipher = ""
 
   message.each_char do |char|
     ord = char.ord
@@ -10,10 +12,10 @@ def caesar_cipher(message, offset)
     # 122 == z
 
     # apply offset only to alphabetical characters (skip spaces, punctuation, etc)
-    if ord.between?(65,90) || ord.between?(97,122)
+    if ord.between?(65, 90) || ord.between?(97, 122)
       # loop from Z to A (or A to Z when offset is negative)
-      base = ord < 91? 65 : 97
-      adjusted_ord = (ord - base + offset) % 26 + base
+      base = ord < 91 ? 65 : 97
+      adjusted_ord = ((ord - base + offset) % 26) + base
 
       cipher += adjusted_ord.chr
     else
